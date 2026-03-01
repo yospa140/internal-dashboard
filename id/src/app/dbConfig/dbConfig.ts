25d54aacfe46394939601388d98bc5a0b76e18ec
Baraa <baraayosry250@gmail.com>
2026-03-01 09:20:40 +0200

    erdfhb

--- id/src/app/dbConfig/dbConfig.ts
+++ id/src/app/dbConfig/dbConfig.ts
@@ -1,24 +1,15 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 export async function connect() {
-    try {
-        mongoose.connect(process.env.MONGO_URI!);
-        const connection = mongoose.connection;
-
-        connection.on('connected', () => {
-            console.log('MongoDB connected successfully');
-        })
-
-        connection.on('error', (err) => {
-            console.log('MongoDB connection error. Please make sure MongoDB is running. ' + err);
-            process.exit();
-        })
-
-    } catch (error) {
-        console.log('Something goes wrong!');
-        console.log(error);
-        
+  try {
+    if (mongoose.connection.readyState >= 1) {
+      return;
     }
 
-
+    await mongoose.connect(process.env.MONGO_URI!);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log("MongoDB connection error", error);
+    throw error;
+  }
 }
